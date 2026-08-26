6851c64eaf380f2148be67dea2122bebadcbbcc4
jichuo1 <jichuo1@users.noreply.github.com>
2026-08-27 06:49:20 +0800

    feat(display): show full numbers

--- Bilibili_Innocent_Lab/app/src/main/java/com/Bilibili_Innocent_Lab/xposedmodule/hook/feature/FeaturePreferences.kt
+++ Bilibili_Innocent_Lab/app/src/main/java/com/Bilibili_Innocent_Lab/xposedmodule/hook/feature/FeaturePreferences.kt
@@ -8,4 +8,5 @@ internal object FeaturePreferences {
     const val BLOCK_APP_UPDATE = "block_app_update"
     const val HIDE_DYNAMIC_CITY_TAB = "hide_dynamic_city_tab"
     const val HIDE_DYNAMIC_SCHOOL_TAB = "hide_dynamic_school_tab"
+    const val SHOW_FULL_NUMBERS = "show_full_numbers"
 }
