10dad3d17da79b1772b030cae702ae997f773a2d
jichuo1 <jichuo1@users.noreply.github.com>
2026-08-27 19:34:03 +0800

    新增了五项功能

---
+++ Bilibili_Innocent_Lab/app/src/test/java/tv/danmaku/bili/splash/ad/model/SplashListResponse.java
@@ -0,0 +1,17 @@
+package tv.danmaku.bili.splash.ad.model;
+
+import java.util.List;
+
+public final class SplashListResponse {
+    public List<Object> getSplashList() {
+        return List.of();
+    }
+
+    public List<Object> getStrategyList() {
+        return List.of();
+    }
+
+    public List<Long> getKeepIds() {
+        return List.of();
+    }
+}
